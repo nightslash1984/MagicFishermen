74ed27ffe23affa4c1a0f110e3439f5c5cfda372
carson.page <halseyaddison@outlook.com>
2026-03-13 13:43:14 -0700

    Basic Menu

---
+++ Magic Fishermen/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
